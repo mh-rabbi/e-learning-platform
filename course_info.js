0a221406a25c4f1934a66ab09106109ece6a9b15
mh-rabbi <mhr221official@gmail.com>
2025-01-08 01:08:09 +0600

    experimental course enroll card design page added

---
+++ course_info.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const enrollButton = document.querySelector('.enroll-button');
+    enrollButton.addEventListener('click', () => {
+        alert('Enrollment process initiated!');
+    });
+
+});
